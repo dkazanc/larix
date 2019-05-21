df30a0ced481b0fa6fc767b78a184d502bb0cb92
Daniil Kazantsev <dkazanc@hotmail.com>
2019-05-21 14:51:04 +0100

    conda_install_upd

--- conda_install.sh
+++ conda_install.sh
@@ -1,5 +1,5 @@
 # conda install for i23seg package
-export i23seg_VERSION=0.1.0
+export VERSION=`date +%Y.%m`
 conda build recipe/ --numpy 1.14 --python 2.7  
-conda install -y -q  i23-seg=${i23seg_VERSION} --use-local --force-reinstall
+conda install -y -q  i23-seg --use-local --force-reinstall
 
