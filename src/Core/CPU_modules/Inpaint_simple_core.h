e4a794b25cb18bbc677699f1fa0d02b3c2a833b6
Daniil Kazantsev <dkazanc@hotmail.com>
2020-11-30 10:44:09 +0000

    new simple inpainter

---
+++ src/Core/CPU_modules/Inpaint_simple_core.h
@@ -0,0 +1,51 @@
+/*
+This work is part of the Core Imaging Library developed by
+Visual Analytics and Imaging System Group of the Science Technology
+Facilities Council, STFC
+
+Copyright 2017 Daniil Kazantsev
+Copyright 2017 Srikanth Nagella, Edoardo Pasca
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include <math.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <stdio.h>
+#include "omp.h"
+#include "utils.h"
+#include "DLSDefines.h"
+
+
+/* C-OMP implementation of simple inpainting shemes
+ * inpainting using averaged interface values
+ *
+ * Input Parameters:
+ * 1. Image/volume to inpaint
+ * 2. Mask of the same size as (1) in 'unsigned char' format  (ones mark the region to inpaint, zeros belong to the data)
+ * 3. Iterations number
+ * 3. sigma - controlling parameter to start inpainting
+ *
+ * Output:
+ * [1] Inpainted image/volume
+ */
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+DLS_EXPORT int Inpaint_simple_CPU_main(float *Input, unsigned char *Mask, float *Output, unsigned char *M_upd, int iterations, float sigma, int dimX, int dimY, int dimZ);
+DLS_EXPORT void linearcomb_inp_2D(float *Input, unsigned char *M_upd, float *Output, float sigma, long dimX, long dimY);
+DLS_EXPORT void linearcomb_inp_3D(float *Input, unsigned char *M_upd, float *Output, float sigma, long dimX, long dimY, long dimZ);
+#ifdef __cplusplus
+}
+#endif
--- src/Python/src/cpu_misc.pyx
+++ src/Python/src/cpu_misc.pyx
@@ -19,6 +19,7 @@ cdef extern int Autocrop_main(float *Input, float *mask_box, float *crop_indeces
 cdef extern int medianfilter_main(float *Input, float *Output, int kernel_size, float mu_threshold, int dimX, int dimY, int dimZ);
 cdef extern int Diffusion_Inpaint_CPU_main(float *Input, unsigned char *Mask, float *Output, float lambdaPar, float sigmaPar, int iterationsNumb, float tau, int penaltytype, int dimX, int dimY, int dimZ);
 cdef extern int NonlocalMarching_Inpaint_main(float *Input, unsigned char *M, float *Output, unsigned char *M_upd, int SW_increment, int iterationsNumb, int trigger, int dimX, int dimY, int dimZ);
+cdef extern int Inpaint_simple_CPU_main(float *Input, unsigned char *Mask, float *Output, unsigned char *M_upd, int iterations, float sigma, int dimX, int dimY, int dimZ);
 #################################################################
 ##########################Autocropper ###########################
 #################################################################
@@ -245,3 +246,53 @@ def INPAINT_NM_2D(np.ndarray[np.float32_t, ndim=2, mode="c"] inputData,
         return (outputData, maskData_upd)
     else:
         raise ValueError("2D CPU nonlocal marching inpainting failed to return 0")
+
+#*********************Inpainting WITH****************************#
+#************simle linear combination method*********************#
+#****************************************************************#
+def INPAINT_LINCOMB(inputData, maskData, iterationsNumb, sigma):
+    if inputData.ndim == 2:
+        return INPAINT_LINCOMB_2D(inputData, maskData, iterationsNumb, sigma)
+    elif inputData.ndim == 3:
+        return INPAINT_LINCOMB_3D(inputData, maskData, iterationsNumb, sigma)
+
+def INPAINT_LINCOMB_2D(np.ndarray[np.float32_t, ndim=2, mode="c"] inputData,
+               np.ndarray[np.uint8_t, ndim=2, mode="c"] maskData,
+               int iterationsNumb,
+               float sigma):
+
+    cdef long dims[2]
+    dims[0] = inputData.shape[0]
+    dims[1] = inputData.shape[1]
+
+    cdef np.ndarray[np.float32_t, ndim=2, mode="c"] outputData = \
+            np.zeros([dims[0],dims[1]], dtype='float32')
+
+    cdef np.ndarray[np.uint8_t, ndim=2, mode="c"] maskData_upd = \
+            np.zeros([dims[0],dims[1]], dtype='uint8')
+
+    if (Inpaint_simple_CPU_main(&inputData[0,0], &maskData[0,0], &outputData[0,0], &maskData_upd[0,0], iterationsNumb, sigma, dims[1], dims[0], 1)==0):
+        return (outputData, maskData_upd)
+    else:
+        raise ValueError("2D CPU inpainting failed to return 0")
+
+def INPAINT_LINCOMB_3D(np.ndarray[np.float32_t, ndim=3, mode="c"] inputData,
+               np.ndarray[np.uint8_t, ndim=3, mode="c"] maskData,
+               int iterationsNumb,
+               float sigma):
+
+    cdef long dims[3]
+    dims[0] = inputData.shape[0]
+    dims[1] = inputData.shape[1]
+    dims[2] = inputData.shape[2]
+
+    cdef np.ndarray[np.float32_t, ndim=3, mode="c"] outputData = \
+            np.zeros([dims[0],dims[1],dims[2]], dtype='float32')
+
+    cdef np.ndarray[np.uint8_t, ndim=3, mode="c"] maskData_upd = \
+            np.zeros([dims[0],dims[1],dims[2]], dtype='uint8')
+
+    if (Inpaint_simple_CPU_main(&inputData[0,0,0], &maskData[0,0,0], &outputData[0,0,0], &maskData_upd[0,0,0], iterationsNumb, sigma, dims[2], dims[1], dims[0])==0):
+        return (outputData, maskData_upd)
+    else:
+        raise ValueError("3D CPU inpainting failed to return 0")
