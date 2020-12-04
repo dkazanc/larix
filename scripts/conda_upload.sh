0a4ace60be43f6aceb3c150ce856639e11341351
Daniil Kazantsev <dkazanc@hotmail.com>
2020-12-04 16:37:58 +0000

    actions2

--- scripts/conda_upload.sh
+++ scripts/conda_upload.sh
@@ -1,11 +1,12 @@
 # Only need to change these two variables
+console.log(`This platform is ${process.platform}`);
 PKG_NAME=larix
 USER=dkazanc
-OS=64
+OS=${process.platform}
 
 mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 export VERSION=`date +%Y.%m`
 conda build .
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m`*.tar.bz2 --force
+$CONDA -t $CONDA_UPLOAD_TOKEN upload -u $USER $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m`*.tar.bz2 --force
