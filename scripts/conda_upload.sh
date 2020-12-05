4c6ade59949b2c1ee26aebc0563aef18ebe5e8f7
Daniil Kazantsev <dkazanc@hotmail.com>
2020-12-05 23:47:16 +0000

    action8

--- scripts/conda_upload.sh
+++ scripts/conda_upload.sh
@@ -6,5 +6,6 @@ mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 export VERSION=`date +%Y.%m`
+conda install --yes anaconda-client
 conda build .
-$CONDA/bin/anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER $CONDA_BLD_PATH/$PKG_NAME-`date +%Y.%m`*.tar.bz2 --force
+$CONDA/bin/anaconda upload -u $USER $CONDA_BLD_PATH/$PKG_NAME-`date +%Y.%m`*.tar.bz2 --force
