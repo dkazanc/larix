cafa9a0430e5c2e6ec59e978d3be0f217125c37b
Daniil Kazantsev <dkazanc@hotmail.com>
2020-12-06 00:02:50 +0000

    action9

--- scripts/conda_upload.sh
+++ scripts/conda_upload.sh
@@ -1,6 +1,7 @@
 # Only need to change these two variables
 PKG_NAME=larix
 USER=dkazanc
+OS=linux-64
 
 mkdir ~/conda-bld
 conda config --set anaconda_upload no
@@ -8,4 +9,4 @@ export CONDA_BLD_PATH=~/conda-bld
 export VERSION=`date +%Y.%m`
 conda install --yes anaconda-client
 conda build .
-$CONDA/bin/anaconda upload -u $USER $CONDA_BLD_PATH/$PKG_NAME-`date +%Y.%m`*.tar.bz2 --force
+$CONDA/bin/anaconda upload -u $USER $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m`*.tar.bz2 --force
