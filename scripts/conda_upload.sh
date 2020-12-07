d7f618d9786ec388fb90946893d7ff35174eedbb
Daniil Kazantsev <dkazanc@hotmail.com>
2020-12-07 17:33:56 +0000

    adding more variations of gcc

--- scripts/conda_upload.sh
+++ scripts/conda_upload.sh
@@ -1,4 +1,5 @@
-# Only need to change these two variables
+#!/bin/sh
+
 PKG_NAME=larix
 USER=dkazanc
 OS=linux-64
@@ -10,7 +11,6 @@ export CONDA_BLD_PATH=~/conda-bld
 export VERSION=`date +%Y.%m`
 conda install --yes anaconda-client
 conda build .
-#$CONDA/bin/anaconda upload -u $USER $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m`*.tar.bz2 --force
 
 # upload packages to conda
 find $CONDA_BLD_PATH/$OS -name *.tar.bz2 | while read file
